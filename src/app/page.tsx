690f5191b952dede969892a347a48fc894fc32c9
pomaleski <gabrielpomaleski@gmail.com>
2024-01-03 19:15:24 -0300

    add responsibility to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
 	return (
-		<div className="h-[calc(100vh-60px)] flex justify-between items-center px-52">
-			<div className="flex flex-col gap-2 text-white">
-				<span className="text-lg">Olá pessoas, eu sou</span>
+		<div className="h-[calc(100vh-60px)] flex flex-col lg:flex-row justify-center lg:justify-around items-center px-10 xl:p-0 gap-20">
+			<div className="flex flex-col gap-2 text-white text-center lg:text-start w-96 xl:w-auto">
+				<span className="text-lg w-auto">Olá pessoas, eu sou</span>
 				<h1 className="text-7xl font-bold">Gabriel Pomaleski</h1>
 				<h2 className="text-lg">Desenvolvedor Web</h2>
 			</div>
