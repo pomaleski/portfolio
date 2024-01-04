25ee965ac9855bed5767c0ebb6907232e5c93933
pomaleski <gabrielpomaleski@gmail.com>
2024-01-04 02:01:32 -0300

    add repositories fetch in projects page

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,7 +1,21 @@
 import Card from '@/components/Card'
 
+interface repoProps {
+	name: string
+	description: string
+	html_url: string
+}
+
 interface ProjetosProps {}
-export default function Projetos(props: ProjetosProps) {
+export default async function Projetos(props: ProjetosProps) {
+	const repos = await fetch('https://api.github.com/users/pomaleski/repos', {
+		next: {
+			revalidate: 60 * 60 * 24, // 1 day
+		},
+	}).then((data) => data.json())
+
+	const pinnedRepos = ['spotify-tailwind-clone', 'petshop']
+
 	return (
 		<div className="flex flex-col justify-center m-6 mt-10">
 			<h1 className="ms-4 text-4xl text-white font-bold text-center md:text-start">
@@ -11,77 +25,32 @@ export default function Projetos(props: ProjetosProps) {
 				Principais
 			</h2>
 			<div className="mt-3 flex flex-col items-center md:grid grid-cols-auto-fill gap-6">
-				<Card
-					title="spotify-tailwind-clone"
-					description="Spotify clone com Tailwind CSS, React e TypecScript"
-					link="https://github.com/pomaleski/spotify-tailwind-clone"
-				/>
-				<Card
-					title="teste"
-					description="
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam praesentium,
-					labore quo vitae saepe ab laboriosam quod veniam sapiente, eius voluptas alias porro ut minus
-					possimus perferendis? Ea, beatae perferendis!
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam praesentium,
-					labore quo vitae saepe ab laboriosam quod veniam sapiente, eius voluptas alias porro ut minus
-					possimus perferendis? Ea, beatae perferendis!"
-				/>
-				<Card
-					title="teste"
-					description="
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam praesentium,
-					labore quo vitae saepe ab laboriosam quod veniam sapiente, eius voluptas alias porro ut minus
-					possimus perferendis? Ea, beatae perferendis!"
-				/>
+				{pinnedRepos.map((name: string) => {
+					const repo = repos.find((repo: repoProps) => repo.name === name)
+					return (
+						<Card
+							key={`pinned-${repo.name}`}
+							title={repo.name}
+							description={repo.description}
+							link={repo.html_url}
+						/>
+					)
+				})}
 			</div>
 			<h2 className="ms-4 mt-10 text-2xl text-white text-center md:text-start">
 				Todos
 			</h2>
 			<div className="mt-3 flex flex-col items-center md:grid grid-cols-auto-fill gap-6">
-				<Card
-					title="spotify-tailwind-clone"
-					description="Spotify clone com Tailwind CSS, React e TypecScript"
-					link="https://github.com/pomaleski/spotify-tailwind-clone"
-				/>
-				<Card
-					title="teste"
-					description="
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam praesentium,
-					labore quo vitae saepe ab laboriosam quod veniam sapiente, eius voluptas alias porro ut minus
-					possimus perferendis? Ea, beatae perferendis!
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam praesentium,
-					labore quo vitae saepe ab laboriosam quod veniam sapiente, eius voluptas alias porro ut minus
-					possimus perferendis? Ea, beatae perferendis!"
-				/>
-				<Card
-					title="teste"
-					description="
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam praesentium,
-					labore quo vitae saepe ab laboriosam quod veniam sapiente, eius voluptas alias porro ut minus
-					possimus perferendis? Ea, beatae perferendis!"
-				/>
-				<Card
-					title="spotify-tailwind-clone"
-					description="Spotify clone com Tailwind CSS, React e TypecScript"
-					link="https://github.com/pomaleski/spotify-tailwind-clone"
-				/>
-				<Card
-					title="teste"
-					description="
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam praesentium,
-					labore quo vitae saepe ab laboriosam quod veniam sapiente, eius voluptas alias porro ut minus
-					possimus perferendis? Ea, beatae perferendis!
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam praesentium,
-					labore quo vitae saepe ab laboriosam quod veniam sapiente, eius voluptas alias porro ut minus
-					possimus perferendis? Ea, beatae perferendis!"
-				/>
-				<Card
-					title="teste"
-					description="
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam praesentium,
-					labore quo vitae saepe ab laboriosam quod veniam sapiente, eius voluptas alias porro ut minus
-					possimus perferendis? Ea, beatae perferendis!"
-				/>
+				{repos.map((repo: repoProps) => {
+					return (
+						<Card
+							key={repo.name}
+							title={repo.name}
+							description={repo.description}
+							link={repo.html_url}
+						/>
+					)
+				})}
 			</div>
 		</div>
 	)
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,14 +12,14 @@ export default function Card({
 	link = '',
 }: CardProps) {
 	return (
-		<div className="flex flex-col gap-3 p-5 bg-black bg-opacity-50 w-80 h-60 rounded-lg">
+		<div className="flex flex-col gap-3 p-5 bg-white bg-opacity-10 w-80 h-60 rounded-lg">
 			<div className="text-center text-white cursor-pointer hover:underline">
 				<Link href={link} target="_blank">
 					{title}
 				</Link>
 			</div>
 			<div className="text-sm">
-				{description.length > 250
+				{description?.length > 250
 					? description.substring(0, 250).concat('...')
 					: description}
 			</div>
