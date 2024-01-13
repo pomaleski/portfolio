6cfa58001c77f750b37e046b7b4d89e835f69fb7
pomaleski <gabrielpomaleski@gmail.com>
2024-01-13 01:46:45 -0300

    add summary to about page

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,12 +1,22 @@
 import Education from '@/components/Education'
 
 interface SobreProps {}
+
 export default function Sobre(props: SobreProps) {
 	return (
-		<div className="flex flex-col items-center m-6 mt-10">
-			<h1 className="text-4xl text-white font-bold text-center">Sobre</h1>
-			<div className="mt-10 mx-10 md:mx-32 lg:mx-56 xl:mx-96 text-lg">
-				<div className="text-center">
+		<div className="flex flex-col items-center m-6 mt-10 text-center">
+			<h1 className="text-4xl text-white font-bold">Sobre</h1>
+			<div className="mt-10 mx-10 md:mx-32 lg:mx-56 xl:mx-96 text-lg flex flex-col gap-8">
+				<div>
+					<h2 className="text-2xl text-white">Resumo</h2>
+					<div className="mt-4 text-base md:text-lg">
+						Estou em busca do primeiro emprego. Comecei a estudar programação em
+						2022, fazendo um curso profisionalizante, e gostei muito, atualmente
+						estou cursando Ciência da Computação e aprimorando meu conhecimento
+						em React e TypeScript. Prentendo também estudar Rust ou Java.
+					</div>
+				</div>
+				<div>
 					<h2 className="text-2xl text-white">Educação</h2>
 					<ul className="mt-4 flex flex-col gap-4">
 						<Education
