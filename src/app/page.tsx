8e1edd56cb9bc2ff963bde86541b98f874f18de8
pomaleski <gabrielpomaleski@gmail.com>
2024-01-13 03:19:39 -0300

    add profile image on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import { FaGithub } from 'react-icons/fa'
 
@@ -5,11 +6,19 @@ export default function Home() {
 	return (
 		<div className="h-[calc(100vh-60px)] flex flex-col lg:flex-row justify-center lg:justify-around items-center px-10 xl:p-0 gap-20">
 			<div className="flex flex-col gap-2 text-white text-center lg:text-start w-96 xl:w-auto">
-				<span className="text-lg w-auto">Olá pessoas, eu sou</span>
+				<span className="text-lg w-auto">Olá pessoas, eu sou o</span>
 				<h1 className="text-7xl font-bold">Gabriel Pomaleski</h1>
 				<h2 className="text-lg">Desenvolvedor Web</h2>
 			</div>
-			<div className="w-96 h-96 bg-black rounded-3xl border-2 border-white overflow-hidden"></div>
+			<div className="w-96 h-96 rounded-3xl border-2 border-black shadow-2xl overflow-hidden flex items-center">
+				<Image
+					src="/img/profile-pic.png"
+					alt="Foto de perfil"
+					width={384}
+					height={384}
+					className="object-contain"
+				></Image>
+			</div>
 			<Link
 				className="flex gap-1 items-center absolute right-5 bottom-5 text-sm hover:underline hover:text-white"
 				href="https://github.com/pomaleski/portfolio"
